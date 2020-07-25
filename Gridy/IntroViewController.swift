f822eba50a42a23c9517b431bdd63005f04c703c
Eva Madarasz <tora.buke@gmail.com>
2020-07-25 10:01:33 +0200

    Better code

--- Gridy/IntroViewController.swift
+++ Gridy/IntroViewController.swift
@@ -12,73 +12,46 @@ import UIKit
 
 class IntroViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
-    
-    
-
-   
-
-
-    
-    
-    
     var selectedImage = UIImage()
     var pickerController = UIImagePickerController()
-
-    
-    func segueToApp(sender: AnyObject) -> Void {
-        self.performSegue(withIdentifier: "showEditorView", sender: self)
-
-    }
-        let pickedImages: [UIImage] = [UIImage(named: "tiger")!,UIImage(named: "stonehenge")!,UIImage(named: "books")!,UIImage(named: "church")!,UIImage(named: "machu-picchu")!]
-    
-    @IBAction func pickButton(_ sender: Any) {
-
-        if let _ = pickedImages.randomElement()  {
-        self.selectedImage = UIImage(named: "stonehenge")!
-    
-    pickerController.delegate = self
-               pickerController.dismiss(animated: true, completion: nil)
-               performSegue(withIdentifier: "showEditorView", sender: nil)
-    
-
-            
-        
-    
-            
-    
-    
-        }
-
-    
-    }
-   
-    
-    @IBAction func cameraButton(_ sender: Any) {
-
-        
-        if pickerController.sourceType == .camera  {
-       pickerController.sourceType = .camera
-        present(pickerController, animated: true, completion: nil)
-            } else {
-     let actionController: UIAlertController = UIAlertController(title: "Camera is not available",message: "On the simulator the camera is not available.", preferredStyle: .alert)
-      let cancelAction: UIAlertAction = UIAlertAction(title: "OK", style: .cancel) { action -> Void  in
-        }
-       actionController.addAction(cancelAction)
-    self.present(actionController, animated: true, completion: nil)
-    }
-    }
-    
- @IBAction func libraryButton(_ sender: Any) {
-        pickerController.sourceType = .photoLibrary
-       present(pickerController, animated: true, completion: nil)
-   
-    }
+    let pickedImages: [UIImage] = [UIImage(named: "tiger")!,UIImage(named: "stonehenge")!,UIImage(named: "books")!,UIImage(named:"church")!,UIImage(named: "machu-picchu")!]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         pickerController.delegate = self
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "showEditorView" {
+            if let destinationVC = segue.destination as? EditorViewController {
+                destinationVC.selectedImage = self.selectedImage
+            }
+        }
+    }
+    
+    @IBAction func pickButton(_ sender: Any) {
+        if let myPicture = pickedImages.randomElement()  {
+            self.selectedImage = myPicture
+            performSegue(withIdentifier: "showEditorView", sender: nil)
+        }
+    }
+    
+    @IBAction func cameraButton(_ sender: Any) {
+        if pickerController.sourceType == .camera  {
+            pickerController.sourceType = .camera
+            present(pickerController, animated: true, completion: nil)
+        } else {
+            let actionController: UIAlertController = UIAlertController(title: "Camera is not available",message: "On the simulator the camera is not available.", preferredStyle: .alert)
+            let cancelAction: UIAlertAction = UIAlertAction(title: "OK", style: .cancel) { action -> Void  in
+            }
+            actionController.addAction(cancelAction)
+            self.present(actionController, animated: true, completion: nil)
+        }
+    }
+    
+    @IBAction func libraryButton(_ sender: Any) {
+        pickerController.sourceType = .photoLibrary
+        present(pickerController, animated: true, completion: nil)
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
@@ -86,21 +59,9 @@ class IntroViewController: UIViewController, UIImagePickerControllerDelegate, UI
             self.selectedImage = selectedImage
             pickerController.dismiss(animated: true, completion: nil)
             performSegue(withIdentifier: "showEditorView", sender: nil)
+        } else {
+            pickerController.dismiss(animated: true, completion: nil)
         }
-        pickerController.dismiss(animated: true, completion: nil)
     }
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "showEditorView" {
-            if let destinationVC = segue.destination as? EditorViewController {
-                destinationVC.selectedImage = self.selectedImage
-                
-            }
-            
-        }
-  
-    }
-    
 }
-
-
 
