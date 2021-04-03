41a00d7d97c230d48fddb4a7af513a3cd6f7ca7c
Eva Madarasz <tora.buke@gmail.com>
2021-04-03 02:20:33 +0200

    Changed code and UI

--- Gridy/PlayfieldViewController.swift
+++ Gridy/PlayfieldViewController.swift
@@ -24,6 +24,7 @@ class PlayfieldViewController: UIViewController, UICollectionViewDelegate, UICol
     let defaultImage : UIImage = UIImage(named: "placeHolder")!
     var shuffledArray : [UIImage] = []
     let itemsPerRow: CGFloat = 4
+   // let CGNumber : Int8 = -1
     let collectionViewIdentifier = "PlayfieldCell"
     let gameCollectionViewIdentifier = "GameCell"
     var gameTimer: Timer?
@@ -186,11 +187,11 @@ class PlayfieldViewController: UIViewController, UICollectionViewDelegate, UICol
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         if collectionView == shuffledCollectionView {
-            return CGSize(width: 59, height: 59)
+            return CGSize(width: 53, height: 53)
         } else {
             let collectionViewWidth : CGFloat = collectionView.frame.width
             let widthPerItem : CGFloat = collectionViewWidth / CGFloat(itemsPerRow)  // find an other solution
-            return CGSize(width: widthPerItem, height: widthPerItem)  // redo it
+            return CGSize(width: widthPerItem - 1.0, height: widthPerItem - 1.0)  // redo it
         }
     }
     
